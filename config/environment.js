00b8f9426a7f75bdaca656442ffc9a2612af2a59
mrf0n <vanka.perm@mail.ru>
2022-07-15 14:39:44 +0500

    fix rooturl

--- config/environment.js
+++ config/environment.js
@@ -45,6 +45,9 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+  }
+  if (environment === 'ci') {
+    // here you can enable a production-specific feature
     ENV.rootURL='/home-work2/';
   }
 
